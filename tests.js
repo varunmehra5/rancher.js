66330f815724e9ef0e21ab410202c9430b589ff4
Rudraksh MK <rudimk.cloud@gmail.com>
2018-05-07 04:06:24 +0530

    Added a test case for checking getEnvironments, and modified the test cases for checking Rancher stacks and associated services.

--- tests.js
+++ tests.js
@@ -4,25 +4,40 @@ const rancher = require('./lib.js')
 describe("Rancher.js tests: ", function(){
 	const cattleRustler = new rancher.Rancher(process.env.BASE_URL, process.env.API_KEY, process.env.API_SECRET)
 
-	it("Get a list of all Rancher stacks", function(){
-		cattleRustler.getStacks()
+	it("Get a list of all Rancher environments", function(){
+		cattleRustler.getEnvironments()
 		.then((result) => {
-			for(let stack of result){
-			expect(stack['id']).to.be.a('string')
-			console.log(`Stack ID: ${stack['id']} || Stack Name: ${stack['name']}`)
+			for(let environment of result){
+				expect(environment['id']).to.be.a('string')
+				console.log(`Environment ID: ${environment['id']} || Environment Name: ${environment['name']}`)
 			}
 		})
 	})
 
+	it("Get a list of all Rancher stacks", function(){
+		cattleRustler.getEnvironments()
+		.then((environmentResult) => {
+			cattleRustler.getStacks(environmentResult[0]['id'])
+			.then((stackResult) => {
+				for(let stack of stackResult){
+				expect(stack['id']).to.be.a('string')
+				console.log(`Stack ID: ${stack['id']} || Stack Name: ${stack['name']}`)
+			}})
+		})
+	})
+
 	it("Get a list of all services in a random Rancher stack", function(){
-		cattleRustler.getStacks()
-		.then((stackResult) => {
-			cattleRustler.getStackServices(stackResult[0]['id'])
+		cattleRustler.getEnvironments()
+		.then((environmentResult) => {
+			cattleRustler.getStacks(environmentResult[0]['id'])
+			.then((stackResult) => {
+				cattleRustler.getStackServices(stackResult[0]['id'])
 			.then((serviceResult) => {
 				for(let service of serviceResult){
 					expect(service['id']).to.be.a('string')
 					console.log(`Service ID: ${service['id']} || Service Name: ${service['name']}`)
 				}
+			})
 			})
 		})
 	})
